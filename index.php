9318c41ce63ea01b321a7dd041f49a3d3eb114eb
Cody Pratorius <codypratorius@users.noreply.github.com>
2020-03-11 13:31:35 -0500

    Major CSS and Home Menu Updates
    Updated menu layout and CSS

--- index.php
+++ index.php
@@ -17,18 +17,29 @@ header("Location: login.php");
 </head>
     <p><strong>Welcome!</strong> You are logged in as <strong><?php echo $_SESSION['firstname']." ".$_SESSION['lastname']; ?></strong></p>
     
-    <center>
-                <h3>Menu</h3>
-                    <ul>
-                        <li><a href="addmovie.php">Add New Movie</a></li>
-                        <li><a href="mymovies.php">My Movies</a></li>
-                    </ul>
-    </center
-    
-    <P>
-    Click <a href="logout.php">here</a> to logout
-    
+           
 
+<table class="nav">
+<tr class="navcontent">
+<td colspan=3"><h3>Menu</h3></td>
+</tr>
+<tr class="navcontent">
+<td class="navimg"><img src="img/lib.png" /></td>
+<td class="navspacer"></td>
+<td class="navimg"><img src="img/add.png" /></td>
+</tr>
+<tr class="navcontent">
+<td class="navlink"><a href="addmovie.php">Add New Movie</a></td>
+<td class="navspacer"></td>
+<td class="navlink"><a href="mymovies.php">My Movies</a></td>
+</tr>
+
+</table>
+    
+<br /><br />
+    <p align="center">
+    Click <a href="logout.php">here</a> to logout
+    </p>
     
 
  <?php } ?>
