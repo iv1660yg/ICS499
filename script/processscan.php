769588297a1cafb8428d02b1b044b63df17315a0
iv1660yg <iv1660yg@metrostate.edu>
2020-04-29 00:54:15 -0500

    Update processscan.php

--- script/processscan.php
+++ script/processscan.php
@@ -3,7 +3,7 @@
 //create short variable names
 $barcode =$_POST['isbn_input'];
 
-$url = "https://api.upcdatabase.org/product/".$barcode."?apikey=67BDE043D668B6EE5508863B7441C873";
+$url = "https://api.upcdatabase.org/product/'".$barcode."'?apikey=67BDE043D668B6EE5508863B7441C873";
 
 $myJSON = json_encode($url);
 
