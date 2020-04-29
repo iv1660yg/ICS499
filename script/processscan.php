c20c04155383538170035fed05b66d54a032fdf0
iv1660yg <iv1660yg@metrostate.edu>
2020-04-29 01:38:16 -0500

    Update processscan.php

--- script/processscan.php
+++ script/processscan.php
@@ -3,9 +3,9 @@
 // set barcode
 $bar=$_POST['isbn_input'];
 
-echo $bar;
+echo $barcode;
 //set upc api url
-$url = "https://api.upcdatabase.org/product/".$bar."?apikey=67BDE043D668B6EE5508863B7441C873";
+$url = "https://api.upcdatabase.org/product/'".$barcode."'?apikey=67BDE043D668B6EE5508863B7441C873";
 
 //call api
 $json = file_get_contents($url);
