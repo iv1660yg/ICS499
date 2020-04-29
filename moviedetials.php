75dc48a93127da39b74492761c2b68eb19a2dfca
iv1660yg <iv1660yg@metrostate.edu>
2020-04-29 12:05:38 -0500

    Update moviedetials.php

--- moviedetials.php
+++ moviedetials.php
@@ -10,7 +10,6 @@ if (!empty($_GET['id'])) {
     //set upc api url
     $url = "https://api.themoviedb.org/3/movie/".$moviedb_ID."?api_key=5a846dc3f5db35f3d5590b415612624c";
 
-    echo $url;
     
     
     //call api
@@ -18,10 +17,60 @@ if (!empty($_GET['id'])) {
     $json = json_decode($json);
     
     
-    echo $json->original_title;
-    echo $json->overview;
+    
+    
 }
 
 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<table class="moviedetail"">
+  <tr>
+    <th colspan="2"><h2>Movie Details</h2></th>
+
+  </tr>
+  <tr>
+    <td>Movie Titles</td>
+    <td><?php echo $json->original_title ?></td>
+  </tr>
+  <tr>
+    <td>Movie Poster</td>
+    <td><img class="resize" src="img/boxart/<?php echo $dbid ?>.jpg"></td>
+  </tr>
+  <tr>
+    <td>Cast</td>
+    <td><?php echo $cast ?></td>
+  </tr>
+  <tr>
+    <tr>
+    <td>Genre</td>
+    <td><?php echo $json->genres[0]->name ?></td>
+  </tr>
+  <tr>
+    <tr>
+    <td>Release Year</td>
+    <td><?php echo $json->release_date ?></td>
+  </tr>
+  <tr>
+    <tr>
+    <td>Runtime</td>
+    <td><?php echo $json->runtime ?></td>
+  </tr> 
+  <tr>
+    <tr>
+    <td>Movie Plot</td>
+    <td><?php echo $json->overview ?></td>
+  </tr>  
+
+</table>
+ 
+</body>
+</html>
