7cff3a9d98946db1b5b41b35de20ea56519ad223
iv1660yg <iv1660yg@metrostate.edu>
2020-04-29 02:59:31 -0500

    Update addmovie.php

--- addmovie.php
+++ addmovie.php
@@ -23,6 +23,7 @@ if (!empty($_POST['isbn_input'])) {
     $json = json_decode($json);
     
 	$scantitle=$json->description;
+	$scantitle =  trim($scantitle);
 
 	$movidburl = "https://api.themoviedb.org/3/search/movie?api_key=5a846dc3f5db35f3d5590b415612624c&query=$scantitle";
 
