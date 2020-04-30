834160f297f3547aa64841607acf17907e1168c5
iv1660yg <iv1660yg@metrostate.edu>
2020-04-30 12:55:22 -0500

    update library

--- viewlibrary.php
+++ viewlibrary.php
@@ -15,19 +15,19 @@ header("Location: login.php");
 <head>
   <link rel="stylesheet" href="css/style.css">
 </head>
+    <p><strong>Welcome!</strong> You are logged in as <strong><?php echo $_SESSION['firstname']." ".$_SESSION['lastname']; ?></strong></p>
     
-    <table width=600px style="margin-left:auto;margin-right:auto;">
-    <th colspan=2><img src="img/menu.png" /></th>
-    <tr>
-    <td><a href="viewlibrary.php"><img src="img/lib1.png" /></a>/td>
-    <td><a href="managemedia.php"><img src="img/mm.png" /></a></td>
-  
+           
 
+    <table width=600px style="margin-left:auto;margin-right:auto;">
+    <th colspan=2><img src="img/libraryheader-icon.png" /></th>
+    <tr>
+    <td><a href="mymovies.php"><img src="img/managemovie-icon.png" /></a></td>
+    <td><a href="mymuisc.php"><img src="img/managemusic-icon.png" /></a></td>
     </tr>
 
 
     </table>
-
     
 <br /><br />
     <p align="center">
